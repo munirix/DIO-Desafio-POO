eec6d2ff29c3a49034232f0310df61d447b160fd
munirix <munira.osmanmourad@gmail.com>
2023-03-29 14:11:23 -0300

    conteudo com base em aula

---
+++ demo/src/main/java/com/example/Curso.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class Curso extends Conteudo{
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "\nCurso { " +
+                "titulo = '" + getTitulo() + '\'' +
+                ", descricao = '" + getDescricao() + '\'' +
+                ", cargaHoraria = " + cargaHoraria +
+                " }\n";
+    }
+}
